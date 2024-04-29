1cff20b3a680368a80e6e79bc3c82963e9a1766a
Pawanyy <pawanyyy01@gmail.com>
2024-04-29 22:58:01 +0530

    fixed readerbox and added Leaf and TreeNode

--- src/components/JsonReaderBox.jsx
+++ src/components/JsonReaderBox.jsx
@@ -26,6 +26,38 @@ function mapPath(data, prefix = "") {
     return mapPathArray;
 }
 
+function LeafNode({ child }) {
+    return (
+        <button className="block w-full text-start px-1 py-2 border-b-2 focus:bg-blue-100">
+            <span className="text-blue-700 font-semibold">{child.prop}:</span>
+            <span className="ps-2">{child.value}</span>
+        </button>
+    )
+}
+
+function TreeNode({ node }) {
+    return (
+        <details>
+            <summary className="px-1 py-2 text-blue-700 font-semibold border-b-2 hover:cursor-pointer focus:bg-blue-100">
+                <span>{node.prop}:</span>
+            </summary>
+            {node.children && node.children.length > 0 && (
+                <div className="ms-4">
+                    {node.children.map((child, index) => (
+                        <>
+                            {
+                                child.children
+                                    ? (<TreeNode key={index} node={child} />)
+                                    : (<LeafNode key={index} child={child} />)
+                            }
+                        </>
+                    ))}
+                </div>
+            )}
+        </details>
+    );
+}
+
 export default function ReaderBox({ jsonText }) {
 
     const [jsonData, setJsonData] = useState([]);
@@ -37,25 +69,20 @@ export default function ReaderBox({ jsonText }) {
             const absData = mapPath(json, "data");
 
             setJsonData(absData);
-            console.log("RenderJson", json);
+            console.log("RenderJson", absData);
         } catch (error) {
             console.log("Action: JSON Render Data | Error ::> ", error.message)
         }
-    }, [])
+    }, [jsonText])
 
     return (<div className="json-reader-tree">
         <div className="px-2">
-            {jsonData.map(row => {
-                return (<>
-                    {row.children ? (<details>{
-                        <summary className="json-reader-tree-parent json-reader-tree-property-selected">
-                            <span>{row.prop}:</span>
-                        </summary>
-
-                    }</details>) : (<div>a</div>)}
+            {jsonData.map((row, index) => (
+                <>
+                    {row.children ? (<TreeNode key={index} node={row} />) : (<LeafNode key={index} child={row} />)}
                 </>
-                )
-            })}
+            )
+            )}
         </div >
     </div>)
 } 
